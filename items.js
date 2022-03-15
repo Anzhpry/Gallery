ca408c1563b7f824e044f8ada6d5736bba55c0ee
Anzhelika <anzh201419@yandex.ua>
2022-03-15 15:55:57 +0300

    .

---
+++ items.js
@@ -0,0 +1,23 @@
+import image1 from './media/1.jpg'
+import image2 from './media/2.jpg'
+import image3 from './media/3.jpg'
+import image4 from './media/4.gif'
+
+export default [
+    {
+        resource: image1,
+        title: 'Мельница у реки'
+    },
+    {
+        resource: image2,
+        title: 'Горы осенью'
+    },
+    {
+        resource: image3,
+        title: 'Деревья летом'
+    },
+    {
+        resource: image4,
+        title: 'Анимированный котик'
+    },
+]
